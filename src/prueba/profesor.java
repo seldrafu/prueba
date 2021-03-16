8ab7ac927915d7af523a07bbcb7421fd64c53141
agusti <entornos@gmail.com>
2021-03-16 19:23:23 +0100

    implementado los getters and setters

--- src/prueba/profesor.java
+++ src/prueba/profesor.java
@@ -7,4 +7,42 @@ public class profesor {
 	private int telefono;
 	
 	
+	public profesor(String nombre, String dni, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.telefono = telefono;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
 }
